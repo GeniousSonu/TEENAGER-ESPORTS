8a115518b94920f72fa9bc09078f2582e255e4c6
SK SAHINUR ISLAM <67267702+GeniousSonu@users.noreply.github.com>
2024-01-12 01:01:10 +0530

    Add files via upload

---
+++ TEENAGER ESPORTS/assets/js/script.js
@@ -0,0 +1,68 @@
+'use strict';
+
+
+
+const navbar = document.querySelector("[data-navbar]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+const navbarToggler = document.querySelector("[data-nav-toggler]");
+
+navbarToggler.addEventListener("click", function () {
+  navbar.classList.toggle("active");
+  this.classList.toggle("active");
+});
+
+for (let i = 0; i < navbarLinks.length; i++) {
+  navbarLinks[i].addEventListener("click", function () {
+    navbar.classList.remove("active");
+    navbarToggler.classList.remove("active");
+  });
+}
+
+
+
+/**
+ * search toggle
+ */
+
+const searchTogglers = document.querySelectorAll("[data-search-toggler]");
+const searchBox = document.querySelector("[data-search-box]");
+
+for (let i = 0; i < searchTogglers.length; i++) {
+  searchTogglers[i].addEventListener("click", function () {
+    searchBox.classList.toggle("active");
+  });
+}
+
+
+
+/**
+ * header
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 200) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
+
+var typed = new Typed(".typing" , {
+  strings:["eSports", "Gaming", "eSports"],
+  typeSpeed:100,
+  BackSpeed:65,
+  loop:true
+});
+
+
+const pointer = document.getElementById('pointer');
+
+document.addEventListener('mousemove', (event) => {
+  pointer.style.top = event.pageY - 16 + 'px';
+  pointer.style.left = event.pageX - 16 + 'px';
+});
